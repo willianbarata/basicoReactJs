eea4a9318373eef0f2a7c27055f439ca983676e2
willianbarata <willianbarata@gmail.com>
2021-12-19 10:29:17 -0200

    Renderizando Elementos

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = 'Willian';
+const element2 = <h1>Olá Brasil</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function App2() {
+  return (
+    <div>
+      {element}
+      {element2}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App2 />, rootElement);
+
+
+
 reportWebVitals();
