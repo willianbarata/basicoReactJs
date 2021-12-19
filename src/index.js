ed0a60a7bb5db7cfa96d866831dfc6490527bfbb
willianbarata <willianbarata@gmail.com>
2021-12-19 10:47:50 -0200

    Criando componente, rederizando com função

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from './Button';
 
 const element = 'Willian';
 const element2 = <h1>Olá Brasil</h1>
+
+function soma(a,b) {
+  alert(a+b)
+}
 
 function App2() {
   return (
     <div>
       {element}
       {element2}
+      <Button onClick={() => soma(20,20)} name="Willian Barata" />
     </div>
   );
 }
